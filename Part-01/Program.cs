65ba594653a89f9e9ae274eb6a83d2aced13f451
Ashik Khulal <askhulal@gmail.com>
2023-12-19 14:33:27 -0600

    Added part 1 materials

---
+++ Part-01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+
+        public void GetWeekDay(Weekdays weekdays)
+        {
+            int weekday = (int)weekdays;
+        }
+    }
+
+    #region Person
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public virtual string GetFullName()
+        {
+            return string.Format("{0} {1}", FirstName, LastName);
+        }
+    }
+    #endregion Person
+
+    #region Employee
+    public class Employee : Person
+    {
+        public string Role { get; set; }
+
+        public Employee(string firstName, string lastName, string role) : base(firstName, lastName)
+        {
+            Role = role;
+        }
+
+        public new string GetFullName()
+        {
+            return string.Format("Mr. / Mr(s). {0} {1}", FirstName, LastName);
+        }
+    }
+    #endregion Employee
+
+    public enum Weekdays
+    {
+        Sunday = 1,
+        Monday = 2,
+        Tuesday = 3,
+        Wednesday = 4,
+        Thursday = 5,
+        Friday = 6,
+        Saturday = 7
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# C-Sharp-Best-Coding-Practices
+# C# Best Coding Practices:
+
+Welcome to "C# Best Coding Practices" GitHub repository!
+
+## Overview:
+
+
+
+## Getting Started:
+
+### Fork and Clone
+
+1. Fork this repository to your GitHub account.
+2. Clone the forked repository to your local machine:
+
+    ```bash
+    git clone https://github.com/ashikkhulal/C-Sharp-Best-Coding-Practices.git
+    ```
+### Structure:
+
+The repo is structured into 10 separate directories, starting from Part-01 to Part-10. Each day's directory contains tasks, exercises, or learning resources regarding best practices related to C# development:
+
+#### [Part-01](./Part-01):
+
+
+
+
+___
+Happy learning! 🚀
