87dc79e137d53e1aa8b7eb5799dac7fcff210d2a
Ashik Khulal <askhulal@gmail.com>
2023-12-19 16:11:30 -0600

    Added part 3 materials

---
+++ Part-03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //TODO: not so urgent
+
+            //FIXME: more urgent
+
+
+            //int n1 =0, n2= 1, n3 = 2;            
+            int n1 = 0;
+            int n2 = 1;
+            int n3 = 2;
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; }
+        public int Age { get; set; }
+
+        public Person(int id, string name, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            BirthDate = DateTime.Parse(birthDate.ToShortDateString());
+            Age = DateTime.Now.Year - birthDate.Year;
+        }
+    }
+}
